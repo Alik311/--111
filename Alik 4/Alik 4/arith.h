0763ce989e237a3c3340fa7fe50e327fc4a10dc5
Alik311 <Dashdamirovalik@mail.ru>
2021-10-09 15:30:58 +0300

    Задание 4
    Написать программу, в которой описана иерархия классов: Прогрессия (арифметическая, геометрическая). Классы наследники должны иметь поля для хранения параметров прогрессии. Реализовать функции вычисления i-го элемента, суммы элементов до n-го элемента. Продемонстрировать работу всех методов классов, предоставив пользователю выбор типа прогрессии.

---
+++ Alik 4/Alik 4/arith.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include "Progres.h"
+/*
+* \breif класс наследник виртуальной функции ариф. прогресс.
+*/
+class arith : public Progress
+{
+private:
+	/*
+	* \breif параметры класса
+	*/
+	double first;
+	double dif;
+	double n;
+public:
+	/*
+	* \breif конструкто с параметрами
+	*/
+	arith(const double first, const double dif, const double n)
+	{
+		this->first = first;
+		this->dif = dif;
+		this->n = n;
+	}
+	/*
+	* \breif конструкто копирования
+	*/
+	arith(arith& other) = default;
+	/*
+	* \breif деструктор по умолчанию
+	*/
+	~arith() = default;
+	/*
+	* \breif вывод общей информации о прогресии
+	*/
+	friend ostream& operator<<(ostream& os, const arith& obj);
+	/*
+	* \breif н-ый элемент прогресии
+	*/
+	double element() const override;
+	/*
+	* \breif сумма прогресии 
+	*/
+	double sum() const override;
+};
---
+++ Alik 4/Alik 4/geo.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include "Progres.h"
+/*
+* \breif класс наследник виртуальной функции геом. прогресс.
+*/
+class geo : public Progress
+{
+/*
+* \breif параметры класса
+*/
+private:
+	double first;
+	double q;
+	double n;
+public:
+	/*
+	* \breif конструкто с параметрами
+	*/
+	geo(const double first, const double q, const double n)
+	{
+		this->first = first;
+		this->q = q;
+		this->n = n;
+	}
+	/*
+	* \breif конструкто копирования
+	*/
+	geo(geo& other) = default;
+	/*
+	* \breif деструктор по умолчанию
+	*/
+	~geo() = default;
+	/*
+	* \breif вывод общей информации о прогресии
+	*/
+	friend ostream& operator<<(ostream & os, const geo & obj);
+	/*
+	* \breif н-ый элемент прогресии
+	*/
+	double element() const override;
+	/*
+	* \breif сумма прогресии
+	*/
+	double sum() const override;
+	/*
+	* \breif функция для сравнения даблов
+	*/
+	bool double_compare(const double a, const double b) const;
+};
